b2f524d0202676fd05f470696e382f49c4224270
DvorakDavid7 <dvorakdavid7@gmail.com>
2026-02-09 13:35:50 +0100

    remove unnecesary table definition

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,13 +1,4 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { boolean, integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const db = drizzle(process.env.DATABASE_URL!);
-
-export const usersTable = pgTable("users", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 255 }).notNull(),
-    age: integer().notNull(),
-    email: varchar({ length: 255 }).notNull().unique(),
-});
-

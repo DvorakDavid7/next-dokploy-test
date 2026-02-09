25d0d630b22698411790664f4102bd32de92f336
DvorakDavid7 <dvorakdavid7@gmail.com>
2026-02-09 13:28:43 +0100

    add better auth schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
     out: './drizzle',
-    schema: './lib/schema.ts',
+    schema: [
+        './lib/schema.ts',
+        './lib/auth-schema.ts',
+    ],
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,

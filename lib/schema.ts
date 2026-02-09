2185371b897746a2faac88004843d5f3fb5ae7cc
DvorakDavid7 <dvorakdavid7@gmail.com>
2026-02-09 13:59:13 +0100

    pass schema to better auth

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import * as authSchema from './auth-schema';
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema: authSchema });
+export const db = drizzle(process.env.DATABASE_URL!);
